8df9c0ac5cbdc8324a9c55fa2925e918da65876e
hoangbuix <buihoang9b8@gmail.com>
2021-09-19 06:15:28 +0000

    add

--- src/main/java/com/hoangbuix/dev/DevApplication.java
+++ src/main/java/com/hoangbuix/dev/DevApplication.java
@@ -1,14 +1,11 @@
 package com.hoangbuix.dev;
 
-import com.hoangbuix.dev.config.FileUploadConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @RequiredArgsConstructor
-@EnableConfigurationProperties({FileUploadConfig.class})
 public class DevApplication {
     public static void main(String[] args) {
         SpringApplication.run(DevApplication.class, args);
